d788c54398faab69d8ab85b3118d95ab4b15d6ae
Shaanukstar123 <shaanukag0@gmail.com>
2021-11-26 17:09:17 +0000

    Added shifts
    Arithmetic shift right and other shifts added

--- modules/alu.v
+++ modules/alu.v
@@ -2,10 +2,12 @@ module alu(
     input [31:0] a,
     input [31:0] b,
     input logic clk,
+    input [3:0] shift_amount,
     input [5:0] func_code, //opcode is always 0 for this module
     output logic zero,
     output [0:31] out,
-);
+
+); //Do shifts as well
     always_comb begin
         case(func_code):
         default: begin end
@@ -18,7 +20,11 @@ module alu(
         6'b101011: out = (a<b); //Set on less than
         6'b100010: out = $unsigned(a) < $unsigned(b);
         6'b100011: out = $unsigned(a) - $unsigned(b);
-        6'b100110: out = (a^b);
+        6'b100110: out = (a^b); //XOR
+        6'b111010: out = $signed(b) >>> shift_amount; //arithmetic shift right
+        6'b111100: out = b << shift_amount; //Logical Shift left/arithmetic shift left (same operation)
+        6'b111110: out = b >> shift_amount; //Logical Shift right.
+
         zero = (out==0);
 
     endcase
