0abeb2ceb9273210442297c769ccecd0967c8880
Shaanukstar123 <shaanukag0@gmail.com>
2021-12-01 17:21:13 +0000

    Update mult.v

--- modules/mult.v
+++ modules/mult.v
@@ -4,19 +4,22 @@ module mult(
     input logic[31:0] b,
     output logic [31:0] high,
     output logic [31:0] low
-)
-    logic [63:0] multplicand, multc_next; // 3 registers
-    logic [31:0] multp; multp_next
+);
+    logic [63:0] multc, multc_next; // 3 registers
+    logic [31:0] multp, multp_next;
     logic [63:0] prod, prod_next;
     logic [4:0] count, count_next; //forloop for multiplication count
 
-    if (a==0 or b==0) begin
-        assign high = 0;
-        assign low = 0; end
-     ;
+
 
     always_comb begin
+        if (a&b==0) begin //if a or b = 0 then multiplier should just output 0
+            prod = 0;
+            count = 32;
+        end
+
         if (count == 0) begin
+
             multc_next = a;
             multp_next = b;
             prod_next = 0;
@@ -26,16 +29,18 @@ module mult(
             multc_next = multc<<1;
             multp_next = multp >>1;
             count_next = count + 1;
+
+        end
     end
 
     assign low = prod[31:0];
     assign high = prod[63:32];
 
 
-    always_ff (@posedge clk) begin //updates values at positive clock edge after combinatorial stage calculates the values for the next cycle
-        prod <= prod_next
+    always_ff @(posedge clk) begin //updates values at positive clock edge after combinatorial stage calculates the values for the next cycle
+        prod <= prod_next;
         multc <= multc_next;
         multp <= multp_next;
         count <= count_next;
-
     end
+endmodule
