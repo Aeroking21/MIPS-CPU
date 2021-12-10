f8f07ebf259faaf9d115876c77d9996996fa67c7
Aeroking21 <55886814+Aeroking21@users.noreply.github.com>
2021-12-10 15:01:02 +0000

    Update opcode_multu_hi_test_2.asm

--- test/opcode_tests/aryan/opcode_multu_hi_test_2.asm
+++ test/opcode_tests/aryan/opcode_multu_hi_test_2.asm
@@ -1,8 +1,7 @@
-jal $0
-nop
-addiu $s1 $0 0x2A
-addiu $v0 $0 0x2B
-multu $s1 $v0
-mfhi $ v0
+addiu s1 $0 0x0000
+addiu v0 $0 0xFFFF
+multu s1 v0
+mfhi v0
+jr $0
 
-assert v0 == 0x70E
+# assert v0 == 0x70E
