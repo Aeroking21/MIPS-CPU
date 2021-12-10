58020408fc7dfb850969e99b4cbffcb273d1f71b
Shaanukstar123 <55187271+Shaanukstar123@users.noreply.github.com>
2021-12-10 16:09:35 +0000

    Update opcode_test_addu_1.asm

--- test/opcode_tests/opcode_test_addu_1.asm
+++ test/opcode_tests/opcode_test_addu_1.asm
@@ -1,8 +1,8 @@
-lui $s2, 0x0001
-addiu $s1, $s2, 0x1170
-addiu $s3, $s2, 0x01D0
-addu $v0, $s1, $s3
-addiu $v1, $v0, 0x0FA0
+addiu s2 $0 0x0001
+addiu s1 s2 0x1170
+addiu s3 s2 0x01D0
+addu v0 s1 s3
+addu v0 v0 v0
+jr $0
 
-# assert($v1 = 140,000)
-# assert($v0 = 136,000)
+#assert (v0 = 9860)
