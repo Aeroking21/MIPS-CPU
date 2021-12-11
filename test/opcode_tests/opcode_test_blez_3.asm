e4ff61fcb31b9ace0cd98615f4cc6a69f8e6abed
Shaanukstar123 <55187271+Shaanukstar123@users.noreply.github.com>
2021-12-11 17:08:04 +0000

    Update opcode_test_blez_3.asm

--- test/opcode_tests/opcode_test_blez_3.asm
+++ test/opcode_tests/opcode_test_blez_3.asm
@@ -1,9 +1,9 @@
-addiu s0 $0 0xFFFF
-addu s1 $0 $2
-bgez s0 0x2
-addiu v1 $0 0x000A
-addiu s3 v1 0x0005
-addiu v0 s3 0x0003
-jr $0
+addiu $t0, $0, 0x0123
+not $s0, $t0
+blez $s0, 14
+addu $v0, $0, $0
 
-# assert(v == 18)
+
+0x60 : addu $v0, $0, $s0 
+
+# assert($v0 == 0x0123)
