2b9589c526ddd606a00811bb7e457c1931b1ca7b
rana2302 <67081360+rana2302@users.noreply.github.com>
2021-12-12 20:01:11 +0000

    Update opcode_test_lh_1.asm

--- test/opcode_tests/opcode_test_lh_1.asm
+++ test/opcode_tests/opcode_test_lh_1.asm
@@ -1,6 +1,9 @@
-lui $t0.0xf000
-lui $t1.0xfff0
-sw t1.0($t0)
-lh $t2.0x0($t0)
-
-assert $t2 == 0xf000ffff
+addiu s1 $0 0x45
+addiu t1 $0 0x58
+mult s1 t1
+mflo t2
+sw t2 0x100($0)
+lhu v0 0x100($0) 
+sw v0 0x100($0)
+addiu v0 v0 0x0fff
+jr $0
