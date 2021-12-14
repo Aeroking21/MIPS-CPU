deb341c1a2bb04b4a2e7d15ced135baa57699b9b
lupisdupis <61511096+lucianecchi@users.noreply.github.com>
2021-12-14 18:18:16 +0000

    Update opcode_test_div_4.asm

--- OFFICIAL/test/0-Assembly/opcode_test_div_4.asm
+++ OFFICIAL/test/0-Assembly/opcode_test_div_4.asm
@@ -1,8 +1,6 @@
-addiu $s0, $0, 0x0246
-addiu $t0, $0, 0x0002
-
-sll $s1, $s0, 22
-
-mflo $v0
-
-# assert($v0 == -1853882368)
+addiu s0 $0 0x0246
+addiu t0 $0 0x0002
+sll s0 s1 22
+div s1 t0
+jr $0
+mflo v0
