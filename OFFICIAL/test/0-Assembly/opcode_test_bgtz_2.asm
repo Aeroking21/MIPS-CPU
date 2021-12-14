fad0fbb26f830bf713a71aeec6e327189fc4f066
lupisdupis <61511096+lucianecchi@users.noreply.github.com>
2021-12-14 14:48:26 +0000

    Update opcode_test_bgtz_2.asm

--- OFFICIAL/test/0-Assembly/opcode_test_bgtz_2.asm
+++ OFFICIAL/test/0-Assembly/opcode_test_bgtz_2.asm
@@ -1,7 +1,8 @@
-addiu s0 $0 0x123
-bgtz $0 0x02
-addiu s1 $0 0x0002
-addiu v0 s1 0x0008
+addiu s1 $0 0x1
+addiu s0 $0 0x0003
+subu s0 s0 s1
+bgtz s0 0xfffe
+addiu s2 s2 0x1
 jr $0
+addu v0 v0 s2
 
-# assert (v0 == 10)
