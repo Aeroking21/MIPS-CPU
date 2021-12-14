6bb81b4eeee7479a3fc9a2b8d998755599464125
lupisdupis <61511096+lucianecchi@users.noreply.github.com>
2021-12-14 00:28:31 +0000

    Update opcode_test_blez_1.asm

--- OFFICIAL/test/0-Assembly/opcode_test_blez_1.asm
+++ OFFICIAL/test/0-Assembly/opcode_test_blez_1.asm
@@ -1,8 +1,6 @@
-addiu $s0, $0, 0x0123
-blez $0, 14
-addu $v0, $0, $0
-
-
-0x60 : addu $v0, $0, $s0 
-
-# assert($v0 == 0x0123)
+blez $0 0x4
+addiu v0 $0 0x1
+lui v0 0xaaaa
+sll $0 $0 0x0
+jr $0
+addu v0 v0 s0
