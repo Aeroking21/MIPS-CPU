ca0a80ee0438e0897943dc9f0e0672209adf7f6b
lupisdupis <61511096+lucianecchi@users.noreply.github.com>
2021-12-14 12:50:24 +0000

    Update opcode_test_beq_2.asm

--- OFFICIAL/test/0-Assembly/opcode_test_beq_2.asm
+++ OFFICIAL/test/0-Assembly/opcode_test_beq_2.asm
@@ -1,9 +1,7 @@
 addiu s0 $0 0x123
 addiu s1 $0 0x124
 beq s0 s1 0x0002
-addiu v1 $0 0x000A
-addiu s3 v1 0x0001
-addiu v0 s3 0x0002
+addiu v0 $0 0x000A
+addiu v0 v0 0x0001
 jr $0
-
-# assert(v == 13)
+addiu v0 v0 0x0002
