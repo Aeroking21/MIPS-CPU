0dce92c15fca51fd65d38fdfe1d1733ef789242b
Lucia Necchi <lucianecchi01@gmail.com>
2021-12-15 23:26:58 +0100

    other test cases

--- OFFICIAL/test/0-Assembly/opcode_test_lbu_1.asm
+++ OFFICIAL/test/0-Assembly/opcode_test_lbu_1.asm
@@ -1,6 +1,2 @@
-addiu t2 $0 0x00F1
-sb t2 0x100(t2)
-lbu v0 0x100(t2)
 jr $0
-
-# (assert v0 = 241)
+lbu v0 0x102($0)
--- OFFICIAL/test/0-Assembly/opcode_test_lbu_2.asm
+++ OFFICIAL/test/0-Assembly/opcode_test_lbu_2.asm
@@ -1,6 +1,2 @@
-addiu v1 $0 0xF
-sb v1 0x22(v1)
-lbu v0 0x22(v1)
 jr $0
-
-# (assert v0 = 15)
+lbu v0 0x10($0)
