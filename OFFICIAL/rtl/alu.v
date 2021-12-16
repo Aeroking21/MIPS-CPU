4f7f55c056ee0bc0054891ed7b5b8c987f1ef567
lupisdupis <61511096+lucianecchi@users.noreply.github.com>
2021-12-16 11:00:27 +0100

    fixed addu and subu

--- OFFICIAL/rtl/alu.v
+++ OFFICIAL/rtl/alu.v
@@ -30,7 +30,7 @@ module alu(
                 divmult_out[63:32] = $unsigned(op1) % $unsigned(op2); 
                 out = 0; end
 
-            4'b0001: out = $unsigned(op1) + $unsigned(op2); //ADDU
+            4'b0001: out = op1 + op2; //ADDU
             4'b0010: out = ($signed(op1)< $signed(op2)); //Set on less than - SLT
             4'b0011: out = ($unsigned(op1) < $unsigned(op2)); //SLTU
             4'b0100: out = op1&op2;//AND
@@ -40,7 +40,7 @@ module alu(
             4'b1000: out = op2 << op1; // SLL(V) Logical Shift left/(same as ASL))
             4'b1010: out = op2 >> op1; //SRL(V) Logical Shift right 
             4'b1011: out = $signed(op2) >>> op1; //arithmetic shift right SRA(V). Variable- shiftamount value pushed onto op1 if this is the case.
-            4'b1110: out = $unsigned(op1) - $unsigned(op2); //SUBU
+            4'b1110: out = op1 - op2; //SUBU
             
             
     endcase
