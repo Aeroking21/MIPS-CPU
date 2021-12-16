3365a96cb377bed72022055a373e06080b41f7d7
Aeroking21 <55886814+Aeroking21@users.noreply.github.com>
2021-12-16 16:38:58 +0000

    changes

--- OFFICIAL/test_mips_cpu_harvard.sh
+++ OFFICIAL/test_mips_cpu_harvard.sh
@@ -3,21 +3,21 @@ set -eou pipefail
 
 if [[ -f "$1/mips_cpu_harvard.v" ]]; then
 
-  echo "Building in progress....." > /dev/stderr
+  # echo "Building in progress....." > /dev/stderr
 
   # Testing all possible instructions
   if [[ $# == 1 ]]; then
     gcc assemble.cpp -lstdc++ -o assemble.out
     #echo "Proceeding to run all test cases" > /dev/stderr
     chmod +x run_all_testcase.sh
-    ./run_all_testcase.sh $1
+    test/run_all_testcase.sh $1
 
   # Testing only the instructions they provide
   elif [[ $# == 2 ]]; then
     gcc assemble.cpp -lstdc++ -o assemble.out
     #echo "Procedding to run $2 test case " > /dev/stderr
     chmod +x run_one_testcase.sh
-    ./run_one_testcase.sh $1 $2
+    test/run_one_testcase.sh $1 $2
 
   else
     >&2 echo "Invalid arguments"
