945b8863f3c3097a50694549d7c979afc75a1f19
Aeroking21 <55886814+Aeroking21@users.noreply.github.com>
2021-12-16 19:01:47 +0000

    Update run_all_testcase.sh

--- OFFICIAL/test/run_all_testcase.sh
+++ OFFICIAL/test/run_all_testcase.sh
@@ -10,5 +10,5 @@ INSTRUCTIONS="test/0-Assembly/*_1.asm"
 for i in ${INSTRUCTIONS} ; do
     INSTR=$(basename ${i} _1.asm)
     test/run_one_testcase.sh ${1} ${INSTR}
-    echo "------------------------"
+    echo "-----------------------"
 done
